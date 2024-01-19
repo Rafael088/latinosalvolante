54e868bcd84c86b2a1bc673ea6d259a6637f7682
Rafael088 <jerafa088@gmail.com>
2024-01-19 10:14:43 -0500

    first

---
+++ src/app/page.jsx
@@ -0,0 +1,82 @@
+
+import "@/styles/home.css"
+export default function Home() {
+  return (
+    <main>
+     <div className="home">
+            <section className="header">
+            <div className="left">
+                <h1>CONSIGUE TU LICENCIA DE MANERA RAPIDA Y SEGURA</h1>
+                <p>
+                Aumenta tus <b>OPORTUNIDADES LABORALES</b> en los Estados Unidos y conduce las vías con TRANQUILIDAD con estos sencillos pasos:
+                </p>
+                <ul>
+                  <li>Podrás abrir una cuenta bancaria</li>
+                  <li>Podrás llevar tus hijos a la escuela</li>
+                  <li>Obtener póliza de seguro para tu auto</li>
+                  <li>Tranquilidad y confianza para el transporte</li>
+                </ul>
+                <button type="button">
+                  Obtener Licencia
+                </button>
+                <p>En menos de 24 horas tendrás tu tramite listo y lo mejor es que:</p>
+                <b>¡NO REALIZAS EL PAGO HASTA QUE SU LICENCIA ESTE APROBADA!</b>
+            </div>
+            <div className="right">
+              <b>Asegura Tu Licencia Ahora Mismo!</b>
+            </div>
+            </section>
+            <section className="goals">
+              <div className="card">
+
+              </div>
+              <div className="cardt">
+          
+              </div>
+              <div className="card">
+          
+              </div>
+              <div className="cardt">
+          
+              </div>
+            </section>
+            <section className="info">
+              <b>NUESTROS PROCESOS</b>
+              <h2>SENCILLOS PASOS PARA SACAR TU LICENCIA DE CONDUCCIÓN</h2>
+              <div className="cont-cards">
+
+              <div className="card">
+          
+              </div>
+              <div className="card">
+          
+              </div>
+              <div className="card">
+          
+              </div>
+              <div className="card">
+          
+              </div>
+              </div>
+              <h2>¿QUIERES OBTENER TU LICENCIA?</h2>
+              
+              <button type="button">
+              ¡LA QUIERO!
+                </button>
+            </section>
+            <section className="contact">
+              <h2>¿POR QUÉ ELEGIRNOS?</h2>
+              <div className="cont-body">
+                <div className="left">
+                  <b>Somos Latinos al volante</b>
+                </div>
+                <div className="right">
+
+                </div>
+
+              </div>
+            </section>
+        </div>
+    </main>
+  );
+}
